dc3e4115b9ec8f943e4e58913a99641cb42e7605
yu4032 <4032542465@smail.lnu.edu.cn>
2026-09-05 05:24:35 -0400

    test: block implicit endpoint creation during rotation ownership

--- src/test/java/com/hellovoid/liquiddock/LauncherGlassProducerTransitionOwnershipTest.java
+++ src/test/java/com/hellovoid/liquiddock/LauncherGlassProducerTransitionOwnershipTest.java
@@ -12,4 +12,10 @@ public class LauncherGlassProducerTransitionOwnershipTest {
         assertTrue(LauncherGlassProducerTransitionPolicy.workstationCanOwnEndpointTransition(false));
         assertFalse(LauncherGlassProducerTransitionPolicy.workstationCanOwnEndpointTransition(true));
     }
+
+    @Test
+    public void genericEndpointCreationIsBlockedWhileRotationOwnsTransition() {
+        assertTrue(LauncherGlassProducerTransitionPolicy.canCreateProducerEndpoint(false));
+        assertFalse(LauncherGlassProducerTransitionPolicy.canCreateProducerEndpoint(true));
+    }
 }
