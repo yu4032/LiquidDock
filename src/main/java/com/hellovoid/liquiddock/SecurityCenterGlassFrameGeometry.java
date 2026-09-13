d31902fc9946c457b68e610af39943c0be13e97b
yu4032 <4032542465@smail.lnu.edu.cn>
2026-09-13 18:47:53 -0400

    chore: remove Security Center morph probe staging

--- src/main/java/com/hellovoid/liquiddock/SecurityCenterGlassFrameGeometry.java
+++ src/main/java/com/hellovoid/liquiddock/SecurityCenterGlassFrameGeometry.java
@@ -52,7 +52,6 @@ final class SecurityCenterGlassFrameGeometry {
                 throw new IllegalArgumentException("Security Center frame nodes use different roots");
             }
         }
-        SecurityCenterGlassMorphProbe.stageFrame(dock, effectiveBox, apps);
         return new SecurityCenterGlassFrameGeometry(dock, effectiveBox, apps, presentation);
     }
 
