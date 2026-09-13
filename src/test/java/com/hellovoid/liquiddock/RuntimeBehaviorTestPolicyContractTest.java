7e36c401126955cc2d3f1bc6389a38ae16eb33e4
yu4032 <4032542465@smail.lnu.edu.cn>
2026-09-13 11:43:48 -0400

    test(policy): audit Stage runtime architecture reader

--- src/test/java/com/hellovoid/liquiddock/RuntimeBehaviorTestPolicyContractTest.java
+++ src/test/java/com/hellovoid/liquiddock/RuntimeBehaviorTestPolicyContractTest.java
@@ -55,6 +55,7 @@ public class RuntimeBehaviorTestPolicyContractTest {
             "SecurityCenterNoNameAnchorContractTest.java",
             "SecurityCenterPassBlurOwnershipContractTest.java",
             "SecurityCenterScopeContractTest.java",
+            "StageWorkspaceRuntimeContractTest.java",
             "SystemUiHomeTransitionWiringContractTest.java",
             "UserFacingPreferenceSchemaTest.java",
             "WidgetRemoteViewsRootBackgroundContractTest.java",
