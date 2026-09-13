976faae2d1210a3a1afe6be1e7538b298fb84ed5
yu4032 <4032542465@smail.lnu.edu.cn>
2026-09-13 02:11:18 -0400

    fix: keep animated Security Center Dock output root-stable

--- src/main/java/com/hellovoid/liquiddock/SecurityCenterSinkOutputPolicy.java
+++ src/main/java/com/hellovoid/liquiddock/SecurityCenterSinkOutputPolicy.java
@@ -11,6 +11,8 @@ final class SecurityCenterSinkOutputPolicy {
     private SecurityCenterSinkOutputPolicy() {}
 
     static boolean usesRootSpaceOutput(MaterialRole role) {
-        return role == MaterialRole.TOOLBOX || role == MaterialRole.ALL_APPS;
+        return role == MaterialRole.DOCK
+                || role == MaterialRole.TOOLBOX
+                || role == MaterialRole.ALL_APPS;
     }
 }
