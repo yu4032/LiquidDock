f35d8e648246c1a90d60e1a7add3ce3b3023b4a6
yu4032 <4032542465@smail.lnu.edu.cn>
2026-09-13 21:36:10 -0400

    chore(release): bump version to 2.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.hellovoid.liquiddock"
         minSdk = 33
         targetSdk = 37
-        versionCode = 16
-        versionName = "2.3.0"
+        versionCode = 17
+        versionName = "2.4.0"
     }
 
     signingConfigs {
