3eb4e844a0611b8ba302ddff5829f13abb06c12f
yu4032 <4032542465@smail.lnu.edu.cn>
2026-09-15 06:51:22 -0400

    fix: keep handle policy preference independent of config schema

--- src/main/java/com/hellovoid/liquiddock/GboardGlassPreferences.java
+++ src/main/java/com/hellovoid/liquiddock/GboardGlassPreferences.java
@@ -6,15 +6,14 @@ import com.hellovoid.liquiddock.config.ConfigSchema;
 final class GboardGlassPreferences {
     static final String ENABLED_KEY = ConfigSchema.Gboard.ENABLED.name();
     static final String AUTO_RESIZE_AFTER_HANDLE_DRAG_KEY =
-            ConfigSchema.Gboard.AUTO_RESIZE_AFTER_HANDLE_DRAG.name();
+            "liquid_gboard_auto_resize_after_handle_drag";
     static final String BLUR_KEY = ConfigSchema.Gboard.BLUR.name();
     static final String TINT_RED_KEY = ConfigSchema.Gboard.TINT_RED.name();
     static final String TINT_GREEN_KEY = ConfigSchema.Gboard.TINT_GREEN.name();
     static final String TINT_BLUE_KEY = ConfigSchema.Gboard.TINT_BLUE.name();
     static final String TINT_ALPHA_KEY = ConfigSchema.Gboard.TINT_ALPHA.name();
     static final boolean ENABLED_DEFAULT = ConfigSchema.Gboard.ENABLED.uiDefault();
-    static final boolean AUTO_RESIZE_AFTER_HANDLE_DRAG_DEFAULT =
-            ConfigSchema.Gboard.AUTO_RESIZE_AFTER_HANDLE_DRAG.uiDefault();
+    static final boolean AUTO_RESIZE_AFTER_HANDLE_DRAG_DEFAULT = true;
 
     static final class Appearance {
         final boolean enabled;
