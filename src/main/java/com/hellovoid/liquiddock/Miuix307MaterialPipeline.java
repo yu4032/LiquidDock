1bc554e6924c22b578a53865638a48964bb0d648
yu4032 <4032542465@smail.lnu.edu.cn>
2026-09-22 11:30:26 +0100

    fix: remove inferred system wallpaper authority

--- src/main/java/com/hellovoid/liquiddock/Miuix307MaterialPipeline.java
+++ src/main/java/com/hellovoid/liquiddock/Miuix307MaterialPipeline.java
@@ -1,6 +1,5 @@
 package com.hellovoid.liquiddock;
 
-import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
@@ -79,9 +78,6 @@ final class Miuix307MaterialPipeline {
                         try {
                             Object launcher = chain.getThisObject();
                             launcherRef = new WeakReference<>(launcher);
-                            if (launcher instanceof Context) {
-                                LauncherWallpaperFreshnessHook.attachContext((Context) launcher);
-                            }
                             Object hotSeats = HookUtil.getField(launcher, "mHotSeats");
                             hotSeatsRef = new WeakReference<>(hotSeats);
                             View background = resolveBackground(hotSeats);
