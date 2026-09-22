b35d66ae2087a09429e62ade4ef6dc4763fe694f
yu4032 <4032542465@smail.lnu.edu.cn>
2026-09-22 12:55:02 +0100

    revert: remove ineffective Workstation force-resume workaround

--- src/test/java/com/hellovoid/liquiddock/WorkstationProducerPolicyTest.java
+++ src/test/java/com/hellovoid/liquiddock/WorkstationProducerPolicyTest.java
@@ -30,12 +30,6 @@ public class WorkstationProducerPolicyTest {
     }
 
     @Test
-    public void workstationSemanticRefreshMustReassertContinuousProducerAuthority() {
-        assertTrue(WorkstationProducerPolicy.shouldForceWorkspaceResume(true));
-        assertFalse(WorkstationProducerPolicy.shouldForceWorkspaceResume(false));
-    }
-
-    @Test
     public void workstationGeometryChangeRebindsSharedProducer() {
         assertTrue(WorkstationProducerPolicy.shouldRebindForGeometryChange(true, true));
         assertFalse(WorkstationProducerPolicy.shouldRebindForGeometryChange(true, false));
