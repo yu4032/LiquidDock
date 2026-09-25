468648b927c27ef928863cd68719fda858b338f8
yu4032 <4032542465@smail.lnu.edu.cn>
2026-09-25 11:22:08 +0800

    Separate schema fallback from configured chromatic default

--- src/test/java/com/hellovoid/liquiddock/config/ChromaticDefaultsContractTest.java
+++ src/test/java/com/hellovoid/liquiddock/config/ChromaticDefaultsContractTest.java
@@ -6,10 +6,10 @@ import static org.junit.Assert.assertEquals;
 
 public class ChromaticDefaultsContractTest {
     @Test
-    public void currentChromaticDefaultMatchesPrismalWhileHistoricalExportStaysTwo() {
+    public void schemaFallbackStaysPrismalWhileDefaultConfigurationUsesProfile() {
         assertEquals(Integer.valueOf(26), ConfigSchema.Glass.CHROMATIC.uiDefault());
         assertEquals(Integer.valueOf(26), ConfigSchema.Glass.CHROMATIC.runtimeFallback());
         assertEquals(Integer.valueOf(2), ConfigSchema.Glass.CHROMATIC.exportDefault());
-        assertEquals(Integer.valueOf(26), PresetManager.defaultValues().get("liquid_chromatic"));
+        assertEquals(Integer.valueOf(9), PresetManager.defaultValues().get("liquid_chromatic"));
     }
 }
