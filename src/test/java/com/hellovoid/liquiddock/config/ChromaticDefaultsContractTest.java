6493bfa268db2bb7126ba8793e1744a3b3df0d6f
yu4032 <4032542465@smail.lnu.edu.cn>
2026-09-25 10:16:18 +0800

    Update chromatic default contract

--- src/test/java/com/hellovoid/liquiddock/config/ChromaticDefaultsContractTest.java
+++ src/test/java/com/hellovoid/liquiddock/config/ChromaticDefaultsContractTest.java
@@ -6,10 +6,10 @@ import static org.junit.Assert.assertEquals;
 
 public class ChromaticDefaultsContractTest {
     @Test
-    public void currentChromaticDefaultMatchesPrismalWhileHistoricalExportStaysTwo() {
-        assertEquals(Integer.valueOf(26), ConfigSchema.Glass.CHROMATIC.uiDefault());
-        assertEquals(Integer.valueOf(26), ConfigSchema.Glass.CHROMATIC.runtimeFallback());
-        assertEquals(Integer.valueOf(2), ConfigSchema.Glass.CHROMATIC.exportDefault());
-        assertEquals(Integer.valueOf(26), PresetManager.defaultValues().get("liquid_chromatic"));
+    public void currentChromaticDefaultMatchesConfiguredProfile() {
+        assertEquals(Integer.valueOf(9), ConfigSchema.Glass.CHROMATIC.uiDefault());
+        assertEquals(Integer.valueOf(9), ConfigSchema.Glass.CHROMATIC.runtimeFallback());
+        assertEquals(Integer.valueOf(9), ConfigSchema.Glass.CHROMATIC.exportDefault());
+        assertEquals(Integer.valueOf(9), PresetManager.defaultValues().get("liquid_chromatic"));
     }
 }
